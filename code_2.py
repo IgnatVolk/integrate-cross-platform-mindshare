0761f9b28882038a7b6d60c926f3e54c2257dcfe
IgnatVolk <142455386+IgnatVolk@users.noreply.github.com>
2023-12-20 18:41:33 +0200

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,55 @@
+
+import random
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
+def main():
+    return data
+    main()
+
+    data = generate_random_data()
+
+if __name__ == "__main__":
+def generate_random_data():
+
+    const data = generateRandomData();
+
+    }
+    for (let i = 0; i < 10; i++) {
+function main() {
+        console.log("Random Number: " + item);
+function generateRandomData() {
+    return data;
+}
+    data.forEach(item => {
+    });
+main();
+        data.push(Math.floor(Math.random() * 100) + 1);
+}
+    const data = [];
+
+    for item in data:
+
+
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
+    data = generate_random_data()
+if __name__ == "__main__":
+def main():
+
+def generate_random_data():
+        print(f"Random Number: {item}")
+    return data
+    main()public class RandomDataGenerator {
+            data[i] = (int) (Math.random() * 100) + 1;
+    public static void main(String[] args) {
+        for (int item : data) {
+        int[] data = new int[10];
+        for (int i = 0; i < 10; i++) {
+        }
+            System.out.println("Random Number: " + item);
+        }
+
+    }
+
+}
